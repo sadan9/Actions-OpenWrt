5f2fbdeada2ab18aa6562170e3f23c4f01ec4999
sadan9 <sadan9@gmail.com>
2021-01-21 14:31:56 +0800

    a

--- script/official/add-lienol-packages.sh
+++ script/official/add-lienol-packages.sh
@@ -4,6 +4,8 @@ luci_packages=( luci-app-passwall )
 git -C ../ clone --depth 1 https://github.com/xiaorouji/openwrt-passwall lienol-packages
 mkdir -p package/lienol
 
+sed -i 's/$(STAGING_DIR_HOST)\/bin\/upx --lzma --best $(GO_PKG_BUILD_BIN_DIR)\/$(PKG_NAME)//g' ../lienol-packages/trojan-go/Makefile
+
 for i in "${packages[@]}"
 do
     mv ../lienol-packages/$i package/lienol/
@@ -13,3 +15,4 @@ do
     mv ../lienol-packages/$i package/lienol/
 done
 rm ../lienol-packages -rf
+
